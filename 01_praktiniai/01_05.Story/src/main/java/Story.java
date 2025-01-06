65129ea8aa19624c096885ac9e1716a43b595ae7
Deividas Morkunas <deividasmor7@gmail.com>
2025-01-06 12:23:37 +0300

    finish first tasks

--- 01_praktiniai/01_05.Story/src/main/java/Story.java
+++ 01_praktiniai/01_05.Story/src/main/java/Story.java
@@ -7,6 +7,20 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+
+        String res1 = scanner.nextLine();
+
+        System.out.println("What is their job?");
+
+        String res2 = scanner.nextLine();
+
+
+        System.out.println("Here is the story:\n" +
+                "Once upon a time there was " + res1  + ", who was " + res2 + ".\n" +
+                "On the way to work, " + res1 + " reflected on life.\n" +
+                "Perhaps " + res1 + " will not be " + res2 +" forever.");
 
     }
 }
