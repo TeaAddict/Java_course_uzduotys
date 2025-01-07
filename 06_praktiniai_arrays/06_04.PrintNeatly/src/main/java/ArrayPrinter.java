b62793cb06e0d46aca4da78ab5b3f39b4c43c175
Deividas Morkunas <deividasmor7@gmail.com>
2025-01-07 23:35:45 +0200

    add 6 tasks

---
+++ 06_praktiniai_arrays/06_04.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class ArrayPrinter {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        int[] array = {5, 1, 3, 4, 2};
+        printNeatly(array);
+    }
+
+    public static void printNeatly(int[] array) {
+        // Write some code in here
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i + 1 == array.length) break;
+            System.out.print(",");
+        }
+    }
+}
